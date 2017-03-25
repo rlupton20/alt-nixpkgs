178a14cd4df7d3758e322816ec383ef42d9432ce
Richard Lupton <richard.lupton@gmail.com>
2017-03-25 23:58:23 +0000

    Added haskell concurrency libraries
    Centralise nix-expressions.

--- default.nix
+++ default.nix
@@ -3,9 +3,12 @@
 let
   pkgs = import <nixpkgs> { inherit system; };
 
+
   callPackage = pkgs.lib.callPackageWith (pkgs // self);
 
   self = {
+    treeThreads = pkgs.haskellPackages.callPackage ./haskellPackages/tree-threads.nix {};
+    concurrentStack = pkgs.haskellPackages.callPackage ./haskellPackages/concurrent-stack.nix {};
   };
 in
 (pkgs // self)
