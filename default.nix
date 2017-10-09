63bddb171994289ee6a6f7230cbaa1847e242c62
Richard Lupton <richard.lupton@gmail.com>
2017-10-09 18:38:04 +0100

    Refactor to make a package set and overlay

--- default.nix
+++ default.nix
@@ -1,35 +1,7 @@
 { system ? builtins.currentSystem }:
 
 let
-  pkgs = import <nixpkgs> { inherit system; };
-  unstable = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) {};
-
-  callPackage = pkgs.lib.callPackageWith (pkgs // self);
-
-  haskellLibraries = {
-    concurrentStack = pkgs.haskellPackages.callPackage ./haskellPackages/concurrent-stack.nix {};
-    haskellEtcd = pkgs.haskellPackages.callPackage ./haskellPackages/haskell-etcd.nix {};
-    treeThreads = pkgs.haskellPackages.callPackage ./haskellPackages/tree-threads.nix {};
-    tyro = pkgs.haskellPackages.callPackage ./haskellPackages/tyro.nix {};
-  };
-
-  vanguard = {
-    vanguardCore = pkgs.haskellPackages.callPackage ./vanguard/vanguard-core.nix {};
-  };
-
-  toolsets = import ./toolsets/toolsets.nix {};
-
-  miniTools = {
-    csv = unstable.lib.callPackagesWith (unstable //self) ./miniTools/csv.nix {};
-  };
-
-  self = {
-    haskellLibraries = haskellLibraries;
-    vanguard = vanguard;
-    toolsets = toolsets;
-    miniTools = miniTools;
-  };
-
-
+  obelisk = import ./overlay.nix;
+  pkgs = import <nixpkgs> { inherit system; overlays = [ obelisk ]; };
 in
-(pkgs // self)
+  pkgs.obelisk
---
+++ overlay.nix
@@ -0,0 +1,27 @@
+self: super:
+let
+  callPackage = super.lib.callPackageWith (super // self);
+  unstable = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) {};
+
+  haskellLibraries = {
+    concurrentStack = super.haskellPackages.callPackage ./haskellPackages/concurrent-stack.nix {};
+    haskellEtcd = super.haskellPackages.callPackage ./haskellPackages/haskell-etcd.nix {};
+    treeThreads = super.haskellPackages.callPackage ./haskellPackages/tree-threads.nix {};
+    tyro = super.haskellPackages.callPackage ./haskellPackages/tyro.nix {};
+  };
+
+  vanguard = {
+    vanguardCore = super.haskellPackages.callPackage ./vanguard/vanguard-core.nix {};
+  };
+
+  miniTools = {
+    csv = unstable.lib.callPackagesWith (unstable // self) ./miniTools/csv.nix {};
+  };
+
+  allPackages.obelisk = {
+    haskellLibraries = haskellLibraries;
+    vanguard = vanguard;
+    miniTools = miniTools;
+  };
+in 
+allPackages
